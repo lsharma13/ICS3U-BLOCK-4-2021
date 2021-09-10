32501d3a755fb3004d584d776f992ef837de24c2
Lara Sharma <lsharma@bayviewglen.ca>
2021-09-10 14:57:03 -0400

    Variables, Primitive Dta Types, Assignment and Interger

---
+++ Unit One - Introduction to Java/src/week1/ExampleFour.java
@@ -0,0 +1,19 @@
+package week1;
+
+/**
+ * Variables - used to store information / data / value 
+ * Primitive Data Types -> (int / double / char / boolean)
+ * Assignment / Assignment Operator - Assigns a value to a variable (=)
+ */
+
+public class ExampleFour {
+    public static void main(String[] args) {
+        int markOne = 75, markTwo = 80, markThree = 87;     //declared 3 integer variables and assigned them values. 
+
+        int average = (markOne + markTwo + markThree) / 3; 
+
+        System.out.println("The average is: " + average);   //String conactantion (doing something to a String)
+
+
+    }
+}
