a1401cfca1baf5639691f44cb8429689bb732b01
Lara Sharma <lsharma@bayviewglen.ca>
2021-09-10 15:08:25 -0400

    Intial Commit

--- Unit One - Introduction to Java/src/week1/ExampleOne.java
+++ Unit One - Introduction to Java/src/week1/ExampleOne.java
@@ -2,7 +2,7 @@ package week1;
 
 public class ExampleOne {
 public static void main(String[] args) {
-    System.out.println("Welcome to ICS3U - AP");
+    System.out.println("Welcome to ICS3U - AP");    //print until the end of the quotation marks and then enter
     System.out.println("Welcome to ICS3U - AP");
     System.out.println("Welcome to ICS3U - AP");
     
--- Unit One - Introduction to Java/src/week1/ExampleTwo.java
+++ Unit One - Introduction to Java/src/week1/ExampleTwo.java
@@ -2,7 +2,7 @@ package week1;
 
 public class ExampleTwo {
 public static void main(String[] args) {
-    System.out.println("A");
+    System.out.println("A");    //print until the end of the quotation marks and then enter 
     System.out.println("B");
     System.out.println("C");
     System.out.println("D");
