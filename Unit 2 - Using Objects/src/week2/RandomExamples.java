b442a7415eaeb981941c7fe81c0978b340ea8971
Lara Sharma <lsharma@bayviewglen.ca>
2021-09-18 13:04:42 -0400

    String Classes

---
+++ Unit 2 - Using Objects/src/week2/RandomExamples.java
@@ -0,0 +1,19 @@
+    package week2;
+
+    public class RandomExamples {
+       public static void main(String[] args) {
+          // int x = 6.2;
+          // int y = mystery();
+          double z = mystery();
+    
+          String s = "This is a " + 1 + " String";
+          String s2 = "This is another " + true + " String";
+          System.out.println(s2);
+          System.out.println(s);
+    
+       }
+    
+       public static double mystery() {
+          return 3.4;
+       }
+    }
