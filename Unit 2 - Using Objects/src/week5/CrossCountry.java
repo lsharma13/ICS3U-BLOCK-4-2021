d3a630c3595572f225cda4bc4b9c17089f8825bb
Lara Sharma <lsharma@bayviewglen.ca>
2021-10-07 21:22:33 -0400

    Cross Country Project

--- Unit 2 - Using Objects/src/week5/CrossCountry.java
+++ Unit 2 - Using Objects/src/week5/CrossCountry.java
@@ -12,6 +12,7 @@ public class CrossCountry {
 
         in.close(); 
     }
+    // this declares the scanner in the main method, and creates three runners 
 
     private static void processRunner(Scanner in) {
         String firstName, lastName;
@@ -42,6 +43,10 @@ public class CrossCountry {
 
         System.out.println("Finish Time: " + splitThree);
 
+        //this method prompts the user for their first name, last name, mile one and two time and their finish time.
+        //It calls the method subtractTimes below. 
+        //It then displays the name, mile one, split one, split two and finish times back to the user 
+
     }
 
     private static String subtractTimes(String endTime, String startTime) {
@@ -53,6 +58,13 @@ public class CrossCountry {
         return convertToTime(diffInSeconds);
         
     }
+    /**
+     * @param endTime = mile two time 
+     * @param startTime = mile one time
+     * @return = converting the difference in seconds into time 
+     * this method converts start time and end time to seconds, and then uses that to calculate the difference in seconds 
+     */
+
 
     private static String convertToTime(double timeInSeconds) {
        int minutes = getMinutes(timeInSeconds);
@@ -61,35 +73,38 @@ public class CrossCountry {
         return String.format("%d:%06.3f", minutes, seconds); 
     
     }
+    /**
+     * @param timeInSeconds = difference in seconds above 
+     * @return = converts to the preferred format (mm:ss.sss)
+     * this method converts the time in seconds into both minutes and seconds and converts it to the format preferred (mm:ss.sss)
+     */
+    
 
     private static double getSeconds(double timeInSeconds) {
         return (timeInSeconds%60);
     }
+    //this method takes the time in seconds and finds the seconds left once converted to even minutes
 
     private static int getMinutes(double timeInSeconds) {
         return (int) (timeInSeconds/60);
     }
+    //this method takes the time in seconds and finds the minutes (as an integer)
 
     private static double convertToSeconds(String time) {
-        String seconds1 = time.substring(0, time.indexOf(":")); //in minutes
-        String seconds2 = time.substring(time.indexOf(":") + 1);// in seconds 
-        double time1 = Double.parseDouble(seconds1)*60; //convert to seconds 
+        String seconds1 = time.substring(0, time.indexOf(":")); 
+        String seconds2 = time.substring(time.indexOf(":") + 1);
+        double time1 = Double.parseDouble(seconds1)*60; 
         double time2 = Double.parseDouble(seconds2);   
-        double finalTime = time1 + time2;   //add
+        double finalTime = time1 + time2;   
         return finalTime; 
     
-        /**
-         * seconds1 = 5
-         * seconds2 = 07.658
-         * 5:07.658
-         */
     }
+    /**
+     * @param time = the time (endTime or startTime) 
+     * @return = This method finds the index of the minutes and then converts them into doubles and minutes (*60)
+     * It then takes the seconds and converts it into doubles 
+     * It adds them together resulting in the final time 
+     */
     
 }
 
-/**
- * explanation of what it does
- * @param endTime = purpose 
- * @param startTime = purpose
- * @return = purpose 
- */
